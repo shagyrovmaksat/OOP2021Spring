0882a85214fea46a6688aad5c22d9d8f9e9fa45a
Erkebulan <60324363+erkkke@users.noreply.github.com>
2021-05-10 17:21:02 +0600

    Update Admin.java

--- FinalOOP/src/uni/Admin.java
+++ FinalOOP/src/uni/Admin.java
@@ -1,49 +1,45 @@
 package uni;
 
+import java.util.Objects;
+import java.util.Vector;
 
-public class Admin extends Employee {
-    
-    
-    
-    public void seeLogFIles() {
-        //TODO
-    }
-    /**
-    * @return 
-     * @generated
-    */
-    public void addUser() {
-        //TODO
-    }
-    /**
-    * @generated
-    */
-    public removeUser() {
-        //TODO
-    }
-    /**
-    * @generated
-    */
-    public updateUser() {
-        //TODO
-    }
-    /**
-    * @generated
-    */
-    public hashCode() {
-        //TODO
-    }
-    /**
-    * @generated
-    */
-    public equals() {
-        //TODO
-    }
-    /**
-    * @generated
-    */
-    public toString() {
-        //TODO
+public class Admin extends User {
+	
+    public Admin() {
+    	super();
     }
     
+    public Admin(String name, String surname, String username, String password) {
+    	super(name, surname, username, password);
+    }
+    
+    public Vector<LogFile> seeLogFiles() {
+    	return Database.logFiles;
+    }
+    
+    public boolean addUser(User user) {
+    	return Database.users.add(user);
+    }
+    
+    public boolean removeUser(User user) {
+    	return Database.users.remove(user);
+    }
+    
+    //?
+    public boolean updateUser(User user) {
+    	return true;
+    }
+    
+    public int hashCode() {
+    	return super.hashCode();
+    }
+    
+    public boolean equals(Object o) {
+    	return super.equals(o);
+    }
+    
+    //?
+    public String toString() {
+    	return super.hashCode();
+    }
 }
