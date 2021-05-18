19172fc85f3214d8b904efb6fa19fbf22539596f
Adil <akingenious@gmail.com>
2021-05-18 21:39:38 +0600

    Adil

--- FinalOOP/src/uni/LetterMark.java
+++ FinalOOP/src/uni/LetterMark.java
@@ -1,16 +1,16 @@
 package uni;
 
 public enum LetterMark {
-    A,
-    Aminus,
-    Bplus,
-    B,
-    Bminus,
-    Cplus,
-    C,
-    Cminus,
-    Dplus,
-    D,
-    Dminus,
-    F
+    A, // 95-100
+    Aminus, // 90-95
+    Bplus, // 85-90
+    B, // 80-85
+    Bminus, // 75-80
+    Cplus, // 70-75
+    C, // 65-70
+    Cminus, // 60-65
+    Dplus, // 55-60
+    D, // 50-55
+    F, // att. mark for semester is <30
+    FX // att. mark for semester is >30, but fin. exam's mark <20
 }
